9aeb5bbd555bb06a7d0c2225a0dd8cab56b5eb09
vladikkuzn <vladikkuzn1@gmail.com>
2020-08-15 20:45:38 +0300

    Added main.css, favicon.png, main.html moved

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import eel
+import eel, pyowm
+
 eel.init('web')
-eel.start('main.html')
+eel.start('main.html', size = (700, 700))
+
+owm = pyowm.OWM("82d316a00d15abac99bcece89526656b")
+@eel.expose
+def get_weather(place:str) -> str:
+    mgr = owm.weather_manager()
+
+    observation = mgr.weather_at_place(place)
+    w = observation.weather
+
+    temp = w.temperature('celsius')['temp']
+    return str(temp)
