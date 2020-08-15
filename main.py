b174c458635f928b18cfff9c51846e5eae86e82a
vladikkuzn <vladikkuzn1@gmail.com>
2020-08-15 22:17:11 +0300

    App works

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import eel, pyowm
 
-eel.init('web')
-eel.start('main.html', size = (700, 700))
-
 owm = pyowm.OWM("82d316a00d15abac99bcece89526656b")
+
 @eel.expose
-def get_weather(place:str) -> str:
+def get_weather(place):
     mgr = owm.weather_manager()
 
     observation = mgr.weather_at_place(place)
     w = observation.weather
 
     temp = w.temperature('celsius')['temp']
-    return str(temp)
+    return "In " + str(place) + " " + str(temp) + " celsius now"
+
+
+eel.init('web')
+eel.start('main.html')
